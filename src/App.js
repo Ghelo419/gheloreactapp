cd4cc42802f571a8ccc5e0ce7fe3f2eb05f6a09f
Ghelo419 <130741401+Ghelo419@users.noreply.github.com>
2023-11-24 16:11:05 +0800

    adding into codes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="name"> Angelo B. Padilla </h1>
+      <h2 className="title-1"> Implementing CSS </h2>
+    </div>
+  );
 }
 
 export default App;
